13305d78ffda9408f138022c6c38af0316c3d659
gberghoff <gageberghoff@gmail.com>
2022-09-25 21:14:21 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import BasicGlobe from "./components/BasicGlobe.vue";
+</script>
+
+<template>
+  <BasicGlobe />
+</template>
+
+<style>
+#app {
+  padding: 0;
+  margin: 0;
+}
+</style>
---
+++ src/components/BasicGlobe.vue
@@ -0,0 +1,25 @@
+<template>
+  <div ref="globeDiv"></div>
+</template>
+
+<script>
+import Globe from "globe.gl";
+import { ref, onMounted } from "vue";
+
+export default {
+  setup() {
+    const globeDiv = ref(null);
+
+    onMounted(() => {
+      const myGlobe = Globe();
+      myGlobe(globeDiv.value).globeImageUrl(
+        "//unpkg.com/three-globe/example/img/earth-night.jpg"
+      );
+    });
+
+    return {
+      globeDiv,
+    };
+  },
+};
+</script>
